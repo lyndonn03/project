81afa3ac79b0d3e7d09614bad08c1258606ecaa3
unknown <pamintuan.lyndonN@outlook.com>
2018-09-14 21:04:44 -0700

    mmino edit

--- react-project/api_server/config/db.config.js
+++ react-project/api_server/config/db.config.js
@@ -2,7 +2,7 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         user : 'postgres',
-        password: 'lyndonn12',
+        password: 'password',
         database: 'testdb'
     }
 })
